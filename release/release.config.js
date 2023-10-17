5935350bf6b8d9feb12fc9a2caa7db5e31455068
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-10-17 16:52:01 +0200

    feat: add release action

---
+++ release/release.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  "plugins": [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "config": "@1024pix/conventional-changelog-pix",
+        "releaseRules": [
+          { tag: "BUGFIX", release: "patch" },
+          { tag: "BUMP", release: "patch" },
+          { tag: "DOC", release: "patch" },
+          { tag: "TECH", release: "patch" },
+          { tag: "FEATURE", release: "minor" },
+          { tag: "BREAKING_CHANGE", release: "major" },
+        ],
+      }
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        "config": "@1024pix/conventional-changelog-pix"
+      }
+    ],
+    "@semantic-release/changelog",
+    "@semantic-release/github",
+    [
+      "@semantic-release/npm",
+      {
+        "npmPublish": process.env.NPM_PUBLISH === "true",
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        "assets": [
+          "CHANGELOG.md",
+        ],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
+  ]
+}
